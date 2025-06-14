30d0f6be2dc1949f483ad2cc26fc448691f86fa7
ROOTXBOT2 <rootxbot2@gmail.com>
2025-06-14 16:50:50 +0900

    Learn Basic Static, View Engine, API

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,41 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.dto.Hello;
+import org.springframework.boot.autoconfigure.batch.BatchTransactionManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author rua
+ */
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+       model.addAttribute("data", "Hello World");
+    return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(value = "name") String name) {
+        return "Hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(value = "name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // "hello spring"
+    }
+}
---
+++ hello-spring/src/main/java/hello/hello_spring/dto/Hello.java
@@ -0,0 +1,18 @@
+package hello.hello_spring.dto;
+
+/**
+ * @author rua
+ */
+
+public class Hello {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
