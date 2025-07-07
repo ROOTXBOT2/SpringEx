e2cb01cf5c7dda0ff2c70c61a7778040033947eb
ROOTXBOT2 <rootxbot2@gmail.com>
2025-07-07 23:21:12 +0900

    Study BeanLifeCycle Logic

---
+++ core_basic/src/test/java/spring/core_basic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package spring.core_basic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author rua
+ */
+public class BeanLifeCycleTest {
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = context.getBean(NetworkClient.class);
+        context.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        //@Bean(initMethod = "init",destroyMethod = "close")
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
