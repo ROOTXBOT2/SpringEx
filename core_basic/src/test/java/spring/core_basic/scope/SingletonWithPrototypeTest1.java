4a41b970fb2519ae2e1be40ff364ed469299aa5a
ROOTXBOT2 <rootxbot2@gmail.com>
2025-07-23 16:41:18 +0900

    Study Spring ObjectProvider

--- core_basic/src/test/java/spring/core_basic/scope/SingletonWithPrototypeTest1.java
+++ core_basic/src/test/java/spring/core_basic/scope/SingletonWithPrototypeTest1.java
@@ -3,8 +3,12 @@ package spring.core_basic.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -34,27 +38,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = context.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
-
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
 
     }
-
-
 
     @Scope("prototype")
     static class PrototypeBean{
